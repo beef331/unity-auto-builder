85856d1217910edc93097e2898c4554921be010c
Jason <beefers331@gmail.com>
2021-02-10 21:31:35 -0700

    Should properly pull for build

--- src/unity_auto_builder.nim
+++ src/unity_auto_builder.nim
@@ -125,6 +125,7 @@ proc commitMessage(){.thread.} =
 proc resyncBuildFiles(obj: BuildObj) =
   discard execShellCmd(fmt"git -C ./{obj.branch} fetch origin")
   discard execShellCmd(fmt"git -C ./{obj.branch} reset --hard origin/master")
+  discard execShellCmd(fmt"git -C ./{obj.branch} pull")
   let projectPath = fmt"{obj.branch}/{obj.subPath}"
   echo projectPath
   for platform in obj.platforms:
